e4849ef1cc52ae0d6e03f61c0a2403e819978333
Cong Wang <77494072+hydy1943cong@users.noreply.github.com>
2024-10-28 15:00:44 -0400

    modified SearchBooks.jsx

--- Develop/client/src/pages/SearchBooks.jsx
+++ Develop/client/src/pages/SearchBooks.jsx
@@ -7,9 +7,11 @@ import {
   Card,
   Row
 } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+//import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -25,7 +27,7 @@ const SearchBooks = () => {
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
     return () => saveBookIds(savedBookIds);
-  });
+  },[saveBookIds]);
 
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
@@ -58,7 +60,7 @@ const SearchBooks = () => {
       console.error(err);
     }
   };
-
+  const [saveBook] = useMutation(SAVE_BOOK);
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
@@ -72,7 +74,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await saveBook({
+        variables: {bookData: {...bookToSave}},
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
