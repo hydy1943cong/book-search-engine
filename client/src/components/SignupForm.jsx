9f6e4241919dbdae1a7bcb1ffa00897bbff3697d
Cong Wang <77494072+hydy1943cong@users.noreply.github.com>
2024-10-28 19:40:02 -0400

    fix a bug in SignupForm

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -9,7 +9,7 @@ const SignupForm = () => {
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
   // set state for form validation
-  const [validated] = useState(false);
+  const [validated, setValidated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
   const [addUser] = useMutation(ADD_USER);
